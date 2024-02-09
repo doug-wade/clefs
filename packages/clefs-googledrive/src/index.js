4df8427b2a0c2c9eed3f9bea3a21c7108938b16a
Douglas Wade <douglas.b.wade@gmail.com>
2024-02-09 15:00:42 -0800

    feat(#2): Read and write files from Google Drive

--- packages/clefs-googledrive/src/index.js
+++ packages/clefs-googledrive/src/index.js
@@ -1,24 +1,61 @@
-const fs = {};
+import {GoogleAuth} from 'google-auth-library';
+import {drive_v3 as drive} from '@googleapis/drive';
 
 export default class ClefsGoogleDrive {
 	constructor() {
 		this.name = 'googledrive';
-	}
 
-	writeFile(file, data) {
-		return new Promise(resolve => {
-			fs[file] = data;
-			resolve();
+		this.authPromise = new GoogleAuth({
+			scopes: [
+				'https://www.googleapis.com/auth/drive',
+				'https://www.googleapis.com/auth/drive.appdata',
+				'https://www.googleapis.com/auth/drive.file',
+				'https://www.googleapis.com/auth/drive.metadata',
+				'https://www.googleapis.com/auth/drive.metadata.readonly',
+				'https://www.googleapis.com/auth/drive.photos.readonly',
+				'https://www.googleapis.com/auth/drive.readonly',
+			],
+		}).then(auth => {
+			google.options({auth});
 		});
 	}
 
-	readFile(file) {
-		return new Promise((resolve, reject) => {
-			if (fs[file]) {
-				resolve(fs[file]);
-			} else {
-				reject(new Error(`file not found ${file}`));
-			}
-		});
+	async writeFile(file, data) {
+		await this.authPromise;
+
+		return drive.files.create(
+			{
+				name: file,
+				media: {
+					body: new ReadableStream(data),
+				},
+			},
+		);
+	}
+
+	async readFile(file) {
+		await this.authPromise;
+
+		return drive.files
+			.get({file, alt: 'media'}, {responseType: 'stream'})
+			.then(res => {
+				return new Promise((resolve, reject) => {
+					let buffer = '';
+
+					res.data
+					.on('end', () => {
+						resolve(buffer);
+					})
+					.on('error', err => {
+						reject(err);
+					})
+					.on('data', d => {
+						buffer += d;
+					})
+					.catch(error => {
+						reject(error);
+					});
+				});
+			});
 	}
 }
