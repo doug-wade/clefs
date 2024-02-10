ffc3587ff3d9cbc0ffc09def6e08fa3375da1046
Douglas Wade <douglas.b.wade@gmail.com>
2024-02-09 16:03:33 -0800

    feat(#63): improve integration testing (#63)

--- packages/integration-tests/test.js
+++ packages/integration-tests/test.js
@@ -1,58 +1,41 @@
-import clefs from 'clefs';
-import path from 'path';
-import ClefsSimpleObject from 'clefs-simpleobject';
-import ClefsFs from 'clefs-fs';
-import ClefsGoogleDrive from 'clefs-googledrive';
-import {cleanupTmpDir, createTmpDir} from './utils.js';
+import os from 'node:os';
+import path from 'node:path';
 import test from 'ava';
-import os from 'os';
+import clefs from 'clefs';
+import ClefsFs from 'clefs-fs';
+import ClefsLocalStorage from 'clefs-localstorage';
+import ClefsSimpleObject from 'clefs-simpleobject';
+import {cleanupTemporaryDirectory, createTemporaryDirectory, FailingPlugin} from './utils.js';
+// Mock local storage per https://www.npmjs.com/package/mock-local-storage
+// This makes the linter vvvvv mad, so we have to disable a lot of rules.
+global.window = {};
+// eslint-disable-next-line import/first, import/order, import/newline-after-import, import/no-unassigned-import
+import 'mock-local-storage';
+global.window.localStorage = global.localStorage;
 
-const tmpDir = os.tmpdir();
 let folder;
-let layers = [new ClefsSimpleObject(), new ClefsFs(), new ClefsGoogleDrive()];
-
 test.before(async () => {
-	folder = await createTmpDir();
+	folder = await createTemporaryDirectory();
 });
 
-test("Throws on unsupported method", async t => {
-	const fs = clefs([new ClefsSimpleObject()]);
+test.after.always(async () => {
+	await cleanupTemporaryDirectory(folder);
+});
 
-	try {
+test('Throws on unsupported method', t => {
+	const fs = clefs([new ClefsSimpleObject(), new ClefsFs(), new ClefsLocalStorage()]);
+
+	const error = t.throws(() => {
 		fs.renegade('foo', 'bar');
-		t.fail();
-	} catch (e) {
-		t.pass();
-	}
+	}, {instanceOf: TypeError});
+
+	t.is(error.message, 'fs.renegade is not a function');
 });
 
-layers.forEach(layer => {
-	test(`Reads and writes files with the ${layer.name} layer`, async t => {
-		const expected = '# Hello World';
-		const testPath = path.join(os.tmpdir(), 'tmp.md');
-		const fs = clefs([new ClefsSimpleObject()]);
-
-		await fs.writeFile(testPath, expected);
-		const actual = await fs.readFile(testPath);
-
-		t.deepEqual(actual, expected);
-	});
-
-	test(`Accesses files with the ${layer.name} layer`, async t => {
-		const testPath = path.join(os.tmpdir(), 'tmp.md');
-		const fs = clefs([new ClefsSimpleObject()]);
-
-		await fs.writeFile(testPath, '# Hello World');
-		await fs.access(testPath);
-
-		t.pass();
-	});
-})
-
-test("Reads and writes files with all layers", async t => {
+test('Writes and reads files', async t => {
 	const expected = '# Hello World';
 	const testPath = path.join(os.tmpdir(), 'tmp.md');
-	const fs = clefs(layers);
+	const fs = clefs([new ClefsSimpleObject(), new ClefsFs(), new ClefsLocalStorage()]);
 
 	await fs.writeFile(testPath, expected);
 	const actual = await fs.readFile(testPath);
@@ -60,6 +43,35 @@ test("Reads and writes files with all layers", async t => {
 	t.deepEqual(actual, expected);
 });
 
-test.after.always(async () => {
-	await cleanupTmpDir(folder);
-})
+test('Writes and accesses files', async t => {
+	const testPath = path.join(os.tmpdir(), 'tmp.md');
+	const fs = clefs([new ClefsSimpleObject(), new ClefsFs(), new ClefsLocalStorage()]);
+
+	await fs.writeFile(testPath, '# Hello World');
+	await fs.access(testPath);
+
+	t.is(global.localStorage.length, 1);
+});
+
+test('Write fails if one layer fails', async t => {
+	const testPath = path.join(os.tmpdir(), 'tmp.md');
+	const fs = clefs([new ClefsSimpleObject(), new ClefsFs(), new ClefsLocalStorage(), new FailingPlugin()]);
+
+	const error = await t.throwsAsync(async () => {
+		await fs.writeFile(testPath, '# Hello World');
+	});
+
+	t.is(error.message, 'writeFile failed');
+});
+
+test('Read succeeds if one layer fails', async t => {
+	const expected = '# Hello World';
+	const testPath = path.join(os.tmpdir(), 'tmp.md');
+	const simpleObjectPlugin = new ClefsSimpleObject();
+	simpleObjectPlugin.writeFile(testPath, expected);
+
+	const fs = clefs([simpleObjectPlugin, new ClefsFs(), new ClefsLocalStorage(), new FailingPlugin()]);
+	const actual = await fs.readFile(testPath);
+
+	t.deepEqual(actual, expected);
+});
